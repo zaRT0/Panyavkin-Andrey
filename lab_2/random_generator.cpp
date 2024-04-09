b43cc487aad35b531e932fa706ce2e297c93c321
zaRT0 <andrey.panyavkin@mail.ru>
2024-04-09 21:43:47 +0400

    test commit

---
+++ lab_2/random_generator.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <random>
+#include <bitset>
+
+using namespace std;
+
+string generateRandomBinarySequence() {
+    random_device rd;
+    mt19937_64 eng(rd());
+    uniform_int_distribution<unsigned long long> distr;
+
+    string result;
+    result.reserve(128);
+
+    for (int i = 0; i < 2; ++i) {
+        unsigned long long random_num = distr(eng);
+        result += std::bitset<64>(random_num).to_string();
+    }
+
+    return result;
+}
+
+int main() {
+    string random_sequence = generateRandomBinarySequence();
+    cout << "Random binary sequence: " << random_sequence << std::endl;
+    return 0;
+}
