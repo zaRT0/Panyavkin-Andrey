4fcd2697bbb5fc6bf1830c4adcdd01ce3bb4d426
zaRT0 <andrey.panyavkin@mail.ru>
2024-04-14 18:41:27 +0400

    add docstring and try except section

--- lab_2/main.py
+++ lab_2/main.py
@@ -5,64 +5,100 @@ from scipy.special import erfc
 from mpmath import gammainc
 from math import sqrt, factorial
 from work_with_json_file import read_json_file
+from constants import BLOCK_SIZE, PI_VALUES, PATH
+
 
 def frequency_bit_test(sequence: str) -> float:
-    N = len(sequence)
-    Sn = sum(1 if bit == '1' else -1 for bit in sequence) / sqrt(N)
-    p_value = erfc(Sn / sqrt(2))
-    return p_value
+    """
+    frequency bit test function
+    param: sequence as str
+    return: p-value
+    """
+    try:
+        N = len(sequence)
+        Sn = sum(1 if bit == "1" else -1 for bit in sequence) / sqrt(N)
+        p_value = erfc(Sn / sqrt(2))
+        return p_value
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
-def calculate_p_value(sequence: str) -> float:
-    N = len(sequence)
-    proportion_of_ones = sequence.count('1') / N
-    if abs(proportion_of_ones - 0.5) >= 2 / sqrt(N):
-        return 0
-    Vn = sum(1 if sequence[i] != sequence[i + 1] else 0 for i in range(N - 1))
-    p_value = erfc((abs(Vn - 2 * N * proportion_of_ones * (1 - proportion_of_ones))) /
-                   (2 * sqrt(2 * N) * proportion_of_ones * (1 - proportion_of_ones)))
-    return p_value
 
-def calculate_p_value_from_sequence(sequence: str) -> float:
-    block_size = 8
-    pi_values = [0.2148, 0.3672, 0.2305, 0.1875]
-    blocks = [sequence[i:i+block_size] for i in range(0, len(sequence), block_size)]
-    statistics = {'v1': 0, 'v2': 0, 'v3': 0, 'v4': 0}
-    for block in blocks:
-        max_ones = 0
-        current_ones = 0
-        for bit in block:
-            if bit == '1':
-                current_ones += 1
-                max_ones = max(max_ones, current_ones)
+def consecutive_bits_test(sequence: str) -> float:
+    """
+    consecutive bit test function
+    param: sequence as str
+    return: p-value
+    """
+    try:
+        N = len(sequence)
+        proportion_of_ones = sequence.count("1") / N
+        if abs(proportion_of_ones - 0.5) >= 2 / sqrt(N):
+            return 0
+        Vn = sum(1 if sequence[i] != sequence[i + 1] else 0 for i in range(N - 1))
+        p_value = erfc(
+            (abs(Vn - 2 * N * proportion_of_ones * (1 - proportion_of_ones)))
+            / (2 * sqrt(2 * N) * proportion_of_ones * (1 - proportion_of_ones))
+        )
+        return p_value
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def long_sequence_units_test(sequence: str) -> float:
+    """
+    long sequence units test function
+    param: sequence as str
+    return: p-value
+    """
+    try:
+        blocks = [
+            sequence[i : i + BLOCK_SIZE] for i in range(0, len(sequence), BLOCK_SIZE)
+        ]
+        statistics = {"v1": 0, "v2": 0, "v3": 0, "v4": 0}
+        for block in blocks:
+            max_ones = 0
+            current_ones = 0
+            for bit in block:
+                if bit == "1":
+                    current_ones += 1
+                    max_ones = max(max_ones, current_ones)
+                else:
+                    current_ones = 0
+            if max_ones <= 1:
+                statistics["v1"] += 1
+            elif max_ones == 2:
+                statistics["v2"] += 1
+            elif max_ones == 3:
+                statistics["v3"] += 1
             else:
-                current_ones = 0
-        if max_ones <= 1:
-            statistics['v1'] += 1
-        elif max_ones == 2:
-            statistics['v2'] += 1
-        elif max_ones == 3:
-            statistics['v3'] += 1
-        else:
-            statistics['v4'] += 1
-    chi_square = sum(((statistics[f'v{i+1}'] - 16 * pi_values[i]) ** 2) / (16 * pi_values[i]) for i in range(4))
-    p_value = gammainc(1.5, chi_square / 2)
-    return p_value
+                statistics["v4"] += 1
+        chi_square = sum(
+            ((statistics[f"v{i+1}"] - 16 * PI_VALUES[i]) ** 2) / (16 * PI_VALUES[i])
+            for i in range(4)
+        )
+        p_value = gammainc(1.5, chi_square / 2)
+        return p_value
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
-def process_sequences_combined(file_path: str):
-    data = read_json_file(file_path)
-    for language, sequence in data.items():
-        p_value_frequency_bit_test = frequency_bit_test(sequence)
-        p_value_calculate_p_value = calculate_p_value(sequence)
-        p_value_calculate_p_value_from_sequence = calculate_p_value_from_sequence(sequence)
-        print(f"Язык: {language}")
-        print(f"p-value frequency_bit_test: {p_value_frequency_bit_test}")
-        print(f"p-value calculate_p_value: {p_value_calculate_p_value}")
-        print(f"p-value calculate_p_value_from_sequence: {p_value_calculate_p_value_from_sequence}")
-        print()
 
 def main():
-    file_path = "lab_2/binary_sequence.json"
-    process_sequences_combined(file_path)
+    data = read_json_file(PATH)
+    for language, sequence in data.items():
+        p_value_frequency_bit_test = frequency_bit_test(sequence)
+        p_value_consecutive_bits_test = consecutive_bits_test(sequence)
+        p_value_long_sequence_units_test = long_sequence_units_test(sequence)
+        print(f"Язык: {language}")
+        print(f"p-value frequency_bit_test: {p_value_frequency_bit_test}")
+        print(f"p-value calculate_p_value: {p_value_consecutive_bits_test}")
+        print(
+            f"p-value calculate_p_value_from_sequence: {p_value_long_sequence_units_test}"
+        )
+        print()
+
 
 if __name__ == "__main__":
     main()
